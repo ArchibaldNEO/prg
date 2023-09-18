fe8bb2cb0dd2584f3fe863b57fe054ceb683b538
ArchibaldNEO <skvortcov1996@gmail.com>
2023-09-18 10:41:07 +0300

    Done TASK-5000

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Hello world!22");
+
+
+
+    System.out.println("Hello world!");
+    System.out.println("Hello world!22");
     System.out.println("Hello world!323423");
     System.out.println("Hello world!333");
-
-    System.out.println(1);
-    System.out.println(2);
   }
 }
